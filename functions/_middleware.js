385abe29b3ccff1ac16303b8be5e12e4e2de63e8
cherrylein <gerrit.noppel@gmail.com>
2026-07-05 00:57:48 +0200

    Inject v1.6 mobile enhancements

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -7,11 +7,22 @@ export async function onRequest(context) {
   }
 
   const html = await response.text();
-  const scriptTag = '<script src="/sync.js" defer></script>';
+  const headClose = "</head>";
   const bodyClose = "</body>";
-  const injected = html.includes(scriptTag)
-    ? html
-    : html.replace(bodyClose, `  ${scriptTag}\n${bodyClose}`);
+  const mobileCssTag = '<link rel="stylesheet" href="/mobile.css">';
+  const syncScriptTag = '<script src="/sync.js" defer></script>';
+  const enhancementsScriptTag = '<script src="/app-enhancements.js" defer></script>';
+
+  let injected = html;
+  if (!injected.includes(mobileCssTag)) {
+    injected = injected.replace(headClose, `  ${mobileCssTag}\n${headClose}`);
+  }
+  if (!injected.includes(syncScriptTag)) {
+    injected = injected.replace(bodyClose, `  ${syncScriptTag}\n${bodyClose}`);
+  }
+  if (!injected.includes(enhancementsScriptTag)) {
+    injected = injected.replace(bodyClose, `  ${enhancementsScriptTag}\n${bodyClose}`);
+  }
 
   const headers = new Headers(response.headers);
   headers.set("content-type", "text/html; charset=utf-8");
